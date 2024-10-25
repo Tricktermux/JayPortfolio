a9b595959794e1abb5dc0d132a029348978f8a62
atomic-zero <lkpanio25@gmail.com>
2024-10-25 19:46:14 +0800

    gaha

---
+++ index.js
@@ -0,0 +1,14 @@
+
+   const express = require('express');
+   const path = require('path');
+
+   const app = express();
+
+   app.get('/', (req, res) => {
+       res.sendFile(path.join(__dirname, 'portfolio.html'));
+   });
+
+   const PORT = process.env.PORT || 25701;
+   app.listen(PORT, () => {
+       console.log(`Server is running on http://localhost:${PORT}`);
+   });
