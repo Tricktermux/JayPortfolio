04225decfa422d5468c9b49f28e1a8dad85d5bc9
atomic-zero <lkpanio25@gmail.com>
2024-10-26 18:07:05 +0800

    hahah

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
    const app = express();
 
    app.get('/', (req, res) => {
-       res.sendFile(path.join(__dirname, 'portfolio.html'));
+       res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
    });
 
    const PORT = process.env.PORT || 25701;
