7d9d7dc6295a893c70956c22eb3bc11fa3e120d7
atomic-zero <lkpanio25@gmail.com>
2024-10-26 18:45:37 +0800

    ahha

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
-   const express = require('express');
-   const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
-   const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
+});
 
-   app.get('/', (req, res) => {
-       res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
-   });
-
-   const PORT = process.env.PORT || 25701;
-   app.listen(PORT, () => {
-       console.log(`Server is running on http://localhost:${PORT}`);
-   });
+const PORT = process.env.PORT || 25701;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
